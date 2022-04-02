479213a092a113eaad41bffba5d4f1ea287cfe2d
Robin Füllgraf <fuellgrafrobin@gmail.com>
2022-04-02 13:42:30 +0200

    Reduce overhead

--- src/main/java/net/simplyvanilla/nopluginscommand/NoPluginsCommand.java
+++ src/main/java/net/simplyvanilla/nopluginscommand/NoPluginsCommand.java
@@ -10,6 +10,8 @@ import java.util.List;
 
 public final class NoPluginsCommand extends JavaPlugin implements Listener {
 
+    private static final List<String> COMMANDS = Arrays.asList("?", "pl", "about", "version", "ver", "plugins", "bukkit:?", "bukkit:pl", "bukkit:about", "bukkit:version", "bukkit:ver", "bukkit:plugins", "minecraft:pl", "minecraft:plugins", "minecraft:about", "minecraft:version", "minecraft:ver");
+
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
@@ -17,13 +19,14 @@ public final class NoPluginsCommand extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onCommandUse(PlayerCommandPreprocessEvent event) {
-        List<String> commands = Arrays.asList("?", "pl", "about", "version", "ver", "plugins", "bukkit:?", "bukkit:pl", "bukkit:about", "bukkit:version", "bukkit:ver", "bukkit:plugins", "minecraft:pl", "minecraft:plugins", "minecraft:about", "minecraft:version", "minecraft:ver");
-        commands.forEach(all -> {
-            String[] arrCommand = event.getMessage().toLowerCase().split(" ", 2);
-            if (arrCommand[0].equalsIgnoreCase("/" + all.toLowerCase())) {
+        String[] arrCommand = event.getMessage().toLowerCase().split(" ", 2);
+
+        for (String command : COMMANDS) {
+            if (arrCommand[0].equalsIgnoreCase("/" + command)) {
                 event.setCancelled(true);
+                return;
             }
-        });
+        }
     }
 
 }
