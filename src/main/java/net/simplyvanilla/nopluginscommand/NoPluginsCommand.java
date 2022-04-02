19d75a4cba5249ee631fae985b670e08bee13935
Robin Füllgraf <fuellgrafrobin@gmail.com>
2022-04-02 13:55:55 +0200

    Add auto complete cancellation

--- src/main/java/net/simplyvanilla/nopluginscommand/NoPluginsCommand.java
+++ src/main/java/net/simplyvanilla/nopluginscommand/NoPluginsCommand.java
@@ -3,14 +3,15 @@ package net.simplyvanilla.nopluginscommand;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.server.TabCompleteEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public final class NoPluginsCommand extends JavaPlugin implements Listener {
 
     private static final List<String> COMMANDS = Arrays.asList("?", "pl", "about", "version", "ver", "plugins", "bukkit:?", "bukkit:pl", "bukkit:about", "bukkit:version", "bukkit:ver", "bukkit:plugins", "minecraft:pl", "minecraft:plugins", "minecraft:about", "minecraft:version", "minecraft:ver");
+    private static final List<String> NO_AUTO_COMPLETE = new ArrayList<>(COMMANDS); // or Arrays.asList("command", "command2");
 
     @Override
     public void onEnable() {
@@ -26,6 +27,16 @@ public final class NoPluginsCommand extends JavaPlugin implements Listener {
                 event.setCancelled(true);
                 return;
             }
+        }
+    }
+
+    @EventHandler
+    public void onTabComplete(TabCompleteEvent event) {
+        String buffer = event.getBuffer().trim().substring(1);
+        event.getSender().sendMessage(buffer);
+
+        if (NO_AUTO_COMPLETE.contains(buffer)) {
+            event.getCompletions().clear();
         }
     }
 
