9d257a1bfd1396a4b5c9cb9926899168bc60fc58
Robin Füllgraf <75497991+zvwild@users.noreply.github.com>
2022-04-03 22:13:02 +0200

    Disable auto complete for some commands and reduce overhead (#2)
    * Reduce overhead
    
    * Add auto complete cancellation
    
    * Use sets
    
    * Remove debug message

--- src/main/java/net/simplyvanilla/nopluginscommand/NoPluginsCommand.java
+++ src/main/java/net/simplyvanilla/nopluginscommand/NoPluginsCommand.java
@@ -3,12 +3,17 @@ package net.simplyvanilla.nopluginscommand;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.server.TabCompleteEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.Arrays;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public final class NoPluginsCommand extends JavaPlugin implements Listener {
+
+    private static final Set<String> COMMANDS = new HashSet<>(Arrays.asList("?", "pl", "about", "version", "ver", "plugins", "bukkit:?", "bukkit:pl", "bukkit:about", "bukkit:version", "bukkit:ver", "bukkit:plugins", "minecraft:pl", "minecraft:plugins", "minecraft:about", "minecraft:version", "minecraft:ver"));
+    private static final Set<String> NO_AUTO_COMPLETE = new HashSet<>(COMMANDS); // or Arrays.asList("command", "command2");
 
     @Override
     public void onEnable() {
@@ -17,13 +22,27 @@ public final class NoPluginsCommand extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onCommandUse(PlayerCommandPreprocessEvent event) {
-        List<String> commands = Arrays.asList("?", "pl", "about", "version", "ver", "plugins", "bukkit:?", "bukkit:pl", "bukkit:about", "bukkit:version", "bukkit:ver", "bukkit:plugins", "minecraft:pl", "minecraft:plugins", "minecraft:about", "minecraft:version", "minecraft:ver");
-        commands.forEach(all -> {
-            String[] arrCommand = event.getMessage().toLowerCase().split(" ", 2);
-            if (arrCommand[0].equalsIgnoreCase("/" + all.toLowerCase())) {
-                event.setCancelled(true);
-            }
-        });
+        String command = event.getMessage().toLowerCase().split(" ", 2)[0];
+
+        if (command.length() == 1) { // Command is only /
+            return;
+        }
+
+        command = command.substring(1);
+
+        if (COMMANDS.contains(command)) {
+            event.setCancelled(true);
+        }
+
+    }
+
+    @EventHandler
+    public void onTabComplete(TabCompleteEvent event) {
+        String buffer = event.getBuffer().trim().substring(1);
+
+        if (NO_AUTO_COMPLETE.contains(buffer)) {
+            event.getCompletions().clear();
+        }
     }
 
 }
