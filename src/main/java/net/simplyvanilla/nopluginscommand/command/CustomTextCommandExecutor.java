f5abae12436d118aa55bd085f2c41f0bd807c319
Netherwhal <mail@netherwhal.com>
2022-05-12 19:46:28 +0200

    misc fixes

--- src/main/java/net/simplyvanilla/nopluginscommand/command/CustomTextCommandExecutor.java
+++ src/main/java/net/simplyvanilla/nopluginscommand/command/CustomTextCommandExecutor.java
@@ -1,42 +1,44 @@
 package net.simplyvanilla.nopluginscommand.command;
 
+import net.simplyvanilla.nopluginscommand.NoPluginsCommand;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
-import java.util.Set;
 
 public class CustomTextCommandExecutor implements CommandExecutor {
-
-    private final FileConfiguration customTextConfig;
-    private final Set<String> keys;
-
-    public CustomTextCommandExecutor(FileConfiguration customTextConfig) {
-        this.customTextConfig = customTextConfig;
-        this.keys = customTextConfig.getKeys(true);
-    }
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (args.length == 0) {
-            sender.sendMessage("Please provide an argument!");
-            return true;
+            sender.sendMessage(ChatColor.RED + "Please provide an argument!");
+            return false;
         }
 
+        NoPluginsCommand plugin = NoPluginsCommand.getInstance();
+        FileConfiguration customTextConfig = plugin.getCustomTextConfigFile();
+
         String messageName = args[0];
+        ConfigurationSection section = customTextConfig.getConfigurationSection(messageName);
 
-        List<String> lines = customTextConfig.getStringList(messageName + ".lines");
+        if (section == null) {
+            sender.sendMessage(ChatColor.RED + "Please provide a valid argument!");
+            return false;
+        }
 
-        if (keys.contains(messageName + ".page-size")) {
-            int itemsPerPage = customTextConfig.getInt(messageName + ".page-size");
+        List<String> lines = section.getStringList("lines");
+
+        if (section.contains("page-size")) {
+            int itemsPerPage = section.getInt("page-size");
 
             if (itemsPerPage <= 0) {
-                sender.sendMessage("page-size must greater than 0");
-                return true;
+                sender.sendMessage(ChatColor.RED + "page-size must greater than 0");
+                return false;
             }
 
             int page = 0;
@@ -45,12 +47,12 @@ public class CustomTextCommandExecutor implements CommandExecutor {
                 try {
                     page = Math.max(0, Integer.parseInt(args[1]) - 1);
                 } catch (NumberFormatException e) {
-                    sender.sendMessage(args[1] + " is not a valid number!");
-                    return true;
+                    sender.sendMessage(ChatColor.RED + args[1] + " is not a valid number!");
+                    return false;
                 }
             } else if (args.length > 2) {
-                sender.sendMessage("Invalid command syntax!");
-                return true;
+                sender.sendMessage(ChatColor.RED + "Invalid command syntax!");
+                return false;
             }
 
             int startIndex = page * itemsPerPage;
--- src/main/java/net/simplyvanilla/nopluginscommand/command/SuicideCommandExecutor.java
+++ src/main/java/net/simplyvanilla/nopluginscommand/command/SuicideCommandExecutor.java
@@ -1,6 +1,7 @@
 package net.simplyvanilla.nopluginscommand.command;
 
-import org.bukkit.Bukkit;
+import net.simplyvanilla.nopluginscommand.NoPluginsCommand;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -9,23 +10,22 @@ import org.jetbrains.annotations.NotNull;
 
 public class SuicideCommandExecutor implements CommandExecutor {
 
-    private final String messageToBroadcast;
-
-    public SuicideCommandExecutor(String messageToBroadcast) {
-        this.messageToBroadcast = messageToBroadcast;
-    }
-
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (!(sender instanceof Player player)) {
-            sender.sendMessage("This command is only for players!");
-            return true;
+            sender.sendMessage(ChatColor.RED + "This command is only for players!");
+            return false;
         }
 
         player.setHealth(0);
+
+        NoPluginsCommand plugin = NoPluginsCommand.getInstance();
+        String messageToBroadcast = plugin.getSuicideBroadcast();
+
         if (messageToBroadcast != null && !messageToBroadcast.isEmpty()) {
-            Bukkit.broadcastMessage(messageToBroadcast.replaceAll("%name%", player.getName()));
+            plugin.getServer().broadcastMessage(messageToBroadcast.replaceAll("%name%", player.getName()));
         }
+
         return true;
     }
 
