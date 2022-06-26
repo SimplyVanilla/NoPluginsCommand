33c39d18e661b3354a3d8ca2445006a73b82a485
Robin Füllgraf <75497991+zvwild@users.noreply.github.com>
2022-06-26 15:04:59 +0200

    Check permissions for whitelisted commands (#14)
    * Check permissions for whitelisted commands
    
    * Remove unused imports

--- src/main/java/net/simplyvanilla/nopluginscommand/EventsListener.java
+++ src/main/java/net/simplyvanilla/nopluginscommand/EventsListener.java
@@ -1,10 +1,15 @@
 package net.simplyvanilla.nopluginscommand;
 
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerCommandSendEvent;
 import org.bukkit.event.server.TabCompleteEvent;
+
+import java.util.Locale;
 
 public class EventsListener implements Listener {
 
@@ -13,26 +18,49 @@ public class EventsListener implements Listener {
         String command = event.getMessage().toLowerCase().split(" +", 2)[0].replace("/", "");
 
         NoPluginsCommand plugin = NoPluginsCommand.getInstance();
-        if (!plugin.getCommandWhitelist().contains(command)) {
+        Command cmd = resolveCommandAlias(command);
+        Player player = event.getPlayer();
+
+        if (cmd == null || (cmd.getPermission() != null && !player.hasPermission(cmd.getPermission()))
+            || !plugin.getCommandWhitelist().contains(command)) {
             event.setCancelled(true);
         }
     }
 
     @EventHandler
     public void onTabComplete(TabCompleteEvent event) {
-        String buffer = event.getBuffer().trim().split(" +", 2)[0].replace("/", "");
+        if (event.getSender() instanceof Player player) {
+            String buffer = event.getBuffer().trim().split(" +", 2)[0].replace("/", "");
 
-        NoPluginsCommand plugin = NoPluginsCommand.getInstance();
-        if (!plugin.getCommandWhitelist().contains(buffer.toLowerCase())) {
-            event.getCompletions().clear();
+            NoPluginsCommand plugin = NoPluginsCommand.getInstance();
+            Command cmd = resolveCommandAlias(buffer);
+
+            if (cmd == null || (cmd.getPermission() != null && !player.hasPermission(cmd.getPermission()))
+                || !plugin.getCommandWhitelist().contains(buffer.toLowerCase())) {
+                event.getCompletions().clear();
+            }
         }
     }
 
     @EventHandler
     public void onServerSendingCommandsToPlayer(PlayerCommandSendEvent event) {
         NoPluginsCommand plugin = NoPluginsCommand.getInstance();
+        Player player = event.getPlayer();
 
-        event.getCommands().removeIf(command -> !plugin.getCommandWhitelist().contains(command));
+        event.getCommands().removeIf(command -> {
+            Command cmd = resolveCommandAlias(command);
+
+            return cmd == null || (cmd.getPermission() != null && !player.hasPermission(cmd.getPermission()))
+                || !plugin.getCommandWhitelist().contains(command);
+        });
+    }
+
+    private Command resolveCommandAlias(String input) {
+        input = input.toLowerCase(Locale.ROOT);
+        if (Bukkit.getCommandAliases().containsKey(input)) {
+            return resolveCommandAlias(Bukkit.getCommandAliases().get(input)[0].split(" +", 2)[0]);
+        }
+        return Bukkit.getCommandMap().getCommand(input);
     }
 
 }
